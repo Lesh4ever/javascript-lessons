ed5f7c553854708820fd7fc42d362550ee680393
Lesh4ever <leshh4ever@gmail.com>
2023-05-03 12:49:31 +0300

    homework2.3 Done

--- 2.3-events-homework/js/main.js
+++ 2.3-events-homework/js/main.js
@@ -1,34 +1,3 @@
-//const input = document.getElementById("input");
-
-//input.addEventListener("input", function() {
-//  console.log(input.value);
-//});
-
-$("#input").on("click", function(event) {
-    event.preventDefault();
-
-    var text = $("#input").val();
-    //var letters = text.length;
-    console.log(text);
-});
-
-$("#action").on("click", function(event) {
-    event.preventDefault();
-
-    var text = $("#text").val();
-    var letters = text.length;
-    console.log(text);
-    console.log(letters);
-});
-
-$(".square").on("click", function(event) {
-    event.preventDefault();
-
-    var index = $(".square").index(this);
-    console.log(index+1);
-    
-});
-
 $(document).ready(function() {
   
   function calculate(operator) {
@@ -36,23 +5,29 @@ $(document).ready(function() {
     var value1 = parseFloat($('#value1').val());
     var value2 = parseFloat($('#value2').val());
 
-   
-    switch(operator) {
-      case '+':
-        var result = value1 + value2;
-        break;
-      case '-':
-        var result = value1 - value2;
-        break;
-      case '*':
-        var result = value1 * value2;
-        break;
-      case '/':
-        var result = value1 / value2;
-        break;
+    if (isNaN(value1) || isNaN(value2)) {
+      alert("введіть коректні данні");
     }
-   
-    $('#result').val(result);
+    else {
+      switch(operator) {
+        case '+':
+          var result = value1 + value2;
+          break;
+        case '-':
+          var result = value1 - value2;
+          break;
+        case '*':
+          var result = value1 * value2;
+          break;
+        case '/':
+          var result = value1 / value2;
+          break;
+        case '^':
+          var result = Math.pow(value1, value2);
+          break;  
+      }
+      $('#result').val(result);
+    }
   }
 
   
@@ -75,4 +50,11 @@ $(document).ready(function() {
     event.preventDefault();
     calculate('/');
   });
+
+  $('#pow').on("click", function(event) {
+    event.preventDefault();
+    calculate('^');
+  }) 
+
+  
 });
