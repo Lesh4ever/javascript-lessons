cf9e4c3540302b5795663661adc0be20c5f67232
Lesh4ever <118616877+Lesh4ever@users.noreply.github.com>
2023-05-03 11:13:19 +0300

    Domashka3_Done

--- 2.3-events-homework/js/main.js
+++ 2.3-events-homework/js/main.js
@@ -28,3 +28,51 @@ $(".square").on("click", function(event) {
     console.log(index+1);
     
 });
+
+$(document).ready(function() {
+  
+  function calculate(operator) {
+    
+    var value1 = parseFloat($('#value1').val());
+    var value2 = parseFloat($('#value2').val());
+
+   
+    switch(operator) {
+      case '+':
+        var result = value1 + value2;
+        break;
+      case '-':
+        var result = value1 - value2;
+        break;
+      case '*':
+        var result = value1 * value2;
+        break;
+      case '/':
+        var result = value1 / value2;
+        break;
+    }
+   
+    $('#result').val(result);
+  }
+
+  
+  $('#plus').click(function(event) {
+    event.preventDefault();
+    calculate('+');
+  });
+
+  $('#minus').click(function(event) {
+    event.preventDefault();
+    calculate('-');
+  });
+
+  $('#multiply').click(function(event) {
+    event.preventDefault();
+    calculate('*');
+  });
+
+  $('#divide').click(function(event) {
+    event.preventDefault();
+    calculate('/');
+  });
+});
